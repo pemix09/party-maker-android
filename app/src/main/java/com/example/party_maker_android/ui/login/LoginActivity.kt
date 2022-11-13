640261950d09f56fc8a85c2fa31224053ef26572
Przemysław Klejno <pemix09@onet.pl>
2022-11-13 16:43:51 +0100

    Validation on Login screen works

--- app/src/main/java/com/example/party_maker_android/ui/login/LoginActivity.kt
+++ app/src/main/java/com/example/party_maker_android/ui/login/LoginActivity.kt
@@ -3,13 +3,68 @@ package com.example.party_maker_android.ui.login
 import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
 
 import com.example.party_maker_android.R
+import com.example.party_maker_android.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var loginBinding: ActivityLoginBinding
+    private var emailIputValid: Boolean = false
+    private var passwordInputValid: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        loginBinding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(loginBinding.root)
+        SetListeners()
+    }
+
+    private fun SetListeners(){
+        loginBinding.loginButton.setOnClickListener {
+            Login()
+        }
+
+        loginBinding.loginEmailInput.addTextChangedListener {
+            loginBinding.loginEmailInputContainer.helperText = validateEmail()
+            loginBinding.loginButton.isEnabled = emailIputValid && passwordInputValid
+        }
+
+        loginBinding.loginPasswordInput.addTextChangedListener {
+            loginBinding.loginPasswordInputContainer.helperText = validatePassword()
+            loginBinding.loginButton.isEnabled = emailIputValid && passwordInputValid
+        }
+    }
+
+    private fun validatePassword(): String?{
+        val password = loginBinding.loginPasswordInput.text.toString()
+        val passMinLen = 8
+
+        if(password.length < passMinLen){
+            this.passwordInputValid = false
+            return getString(R.string.password_min_length_error_message, passMinLen)
+        }
+
+        this.passwordInputValid = true
+        return null
+    }
+    private fun validateEmail(): String?{
+        val email = loginBinding.loginEmailInput.text.toString()
+
+        if(email.isEmpty()){
+            this.emailIputValid = false
+            return getString(R.string.email_required_error_message)
+        }
+        if(android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches() == false){
+            this.emailIputValid = false
+            return getString(R.string.email_not_valid_error_message)
+        }
+
+        this.emailIputValid = true
+        return null
+    }
+
+    private fun Login(){
+        //here we can
     }
 }
