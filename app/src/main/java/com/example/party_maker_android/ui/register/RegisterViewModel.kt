ff7519dec6bf5d7d6e285aeb8cd28917a66f06f2
Przemysław Klejno <pemix09@onet.pl>
2022-11-22 08:34:27 +0100

    Context in model works, registration validation fields are done

--- app/src/main/java/com/example/party_maker_android/ui/register/RegisterViewModel.kt
+++ app/src/main/java/com/example/party_maker_android/ui/register/RegisterViewModel.kt
@@ -1,6 +1,8 @@
 package com.example.party_maker_android.ui.register
 
+import android.app.Application
 import android.widget.Toast
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
@@ -19,7 +21,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import kotlin.coroutines.coroutineContext
 
-class RegisterViewModel : ViewModel(){
+class RegisterViewModel(application: Application) : AndroidViewModel(application){
     private var isEmailValid: Boolean = false
     private var isNickNameValid: Boolean = false
     private var isPasswordValid: Boolean = false
@@ -32,17 +34,22 @@ class RegisterViewModel : ViewModel(){
     val isFormValid: MutableLiveData<Boolean> = MutableLiveData<Boolean>(false)
 
     fun validateForm(){
-        isFormValid.value = (isEmailValid && isNickNameValid && isPasswordValid && isPasswordConfirmValid)
+        var formValidation = (isEmailValid && isNickNameValid && isPasswordValid && isPasswordConfirmValid)
+        if(isFormValid.value != formValidation){
+            isFormValid.value = formValidation
+        }
     }
 
     fun validateEmail(email: String){
+        val context = getApplication<Application>().applicationContext
+
         if(email.isEmpty()){
             this.isEmailValid = false
-            this.emailValidationMessage.value = ""
+            this.emailValidationMessage.value = context.getString(R.string.email_required_error_message)
         }
         else if(android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches() == false){
             this.isEmailValid = false
-            this.emailValidationMessage.value = "Email not valid"
+            this.emailValidationMessage.value = context.getString(R.string.email_not_valid_error_message)
         }
         else{
             this.isEmailValid = true
@@ -52,20 +59,53 @@ class RegisterViewModel : ViewModel(){
     }
 
     fun validateUserName(userName: String){
-        this.userNameValidationMessage.value = ":)"
-        this.isNickNameValid = true
+        val context = getApplication<Application>().applicationContext
+
+        if(userName.isEmpty()){
+            this.isNickNameValid = false
+            this.userNameValidationMessage.value = context.getString(R.string.username_required)
+        }
+        else{
+            this.isNickNameValid = true
+            this.userNameValidationMessage.value = ""
+        }
         validateForm()
     }
 
     fun validatePassword(password: String){
-        this.isPasswordValid = true
-        this.passwordValidationMessage.value = "da"
+        val context = getApplication<Application>().applicationContext
+        val passMinLen = 8
+
+        if(password.isEmpty()){
+            this.isPasswordValid = false
+            this.passwordValidationMessage.value = context.getString(R.string.login_password_input_required_message)
+        }
+        else if(password.length < passMinLen){
+            this.isPasswordValid = false
+            this.passwordValidationMessage.value = context.getString(R.string.password_min_length_error_message, passMinLen)
+        }
+        else{
+            this.isPasswordValid = true
+            this.passwordValidationMessage.value = ""
+        }
         validateForm()
     }
 
     fun validatePasswordConfirmation(password: String, passwordConfirmation: String){
-        this.isPasswordConfirmValid = true
-        this.passwordConfirmationValidationMessage.value = "dsa"
+        val context = getApplication<Application>().applicationContext
+
+        if(password != passwordConfirmation){
+            this.isPasswordConfirmValid = false
+            this.passwordConfirmationValidationMessage.value = context.getString(R.string.password_confirmation_not_match)
+        }
+        else if(passwordConfirmation.isEmpty()){
+            this.isPasswordConfirmValid = false
+            this.passwordConfirmationValidationMessage.value = context.getString(R.string.password_confirmation_required)
+        }
+        else{
+            this.isPasswordConfirmValid = true
+            this.passwordConfirmationValidationMessage.value = ""
+        }
         validateForm()
     }
 
