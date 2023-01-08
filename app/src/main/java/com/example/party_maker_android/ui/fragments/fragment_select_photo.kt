7bc4aa93f902b1ebc0089880ecd804a82defa3a1
Przemysław Klejno <pemix09@onet.pl>
2023-01-08 13:37:13 +0100

    Making pictures with camera works

--- app/src/main/java/com/example/party_maker_android/ui/fragments/fragment_select_photo.kt
+++ app/src/main/java/com/example/party_maker_android/ui/fragments/fragment_select_photo.kt
@@ -22,12 +22,10 @@ class fragment_select_photo : Fragment() {
     private lateinit var binding: FragmentSelectPhotoBinding
     private val photoGalleryRequestCode: Int = 1
     private val pickPhotoRequestCode: Int = 2
+    private val requestCameraAccessRequestCode: Int = 3
+    private val makePhotoRequestCode: Int = 4
     var selectedPhoto: Uri? = null
     var selectedBitMap: Bitmap? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -44,24 +42,38 @@ class fragment_select_photo : Fragment() {
 
     private fun setButtonClickListener(){
         binding.makePhotoButton.setOnClickListener {
-
+            makePhoto()
         }
         binding.selectFromGalleryButton.setOnClickListener{
-            selectFromGallery(it)
+            selectFromGallery()
         }
     }
 
-    private fun selectFromGallery(view: View){
+    private fun selectFromGallery() {
         activity?.let{
             if(ContextCompat.checkSelfPermission(it.applicationContext, android.Manifest.permission.READ_EXTERNAL_STORAGE)
                 != PackageManager.PERMISSION_GRANTED){
                 requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), photoGalleryRequestCode)
             }
             else{
-                val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-                startActivityForResult(intent, pickPhotoRequestCode)
+                val pickPhoto = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                startActivityForResult(pickPhoto, pickPhotoRequestCode)
             }
         }
+    }
+
+    private fun makePhoto(){
+        activity?.let{
+            if(ContextCompat.checkSelfPermission(it.applicationContext, android.Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED){
+                requestPermissions(arrayOf(android.Manifest.permission.CAMERA), requestCameraAccessRequestCode)
+            }
+            else{
+                val openCamera: Intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                startActivityForResult(openCamera, makePhotoRequestCode)
+            }
+        }
+
     }
 
     override fun onRequestPermissionsResult(
@@ -70,40 +82,44 @@ class fragment_select_photo : Fragment() {
         grantResults: IntArray
     ) {
         if(requestCode == photoGalleryRequestCode){
-            if(grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
                 val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
                 startActivityForResult(intent, pickPhotoRequestCode)
+            }
+        }
+        else if(requestCode == requestCameraAccessRequestCode){
+            if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                val openCamera: Intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                startActivityForResult(openCamera, makePhotoRequestCode)
             }
         }
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        //hide the buttons when photo was selected
+        if(data != null){
+            binding.selectPhotoText.isVisible = false
+            binding.selectFromGalleryButton.isVisible = false
+            binding.makePhotoButton.isVisible = false
+        }
+        //photo from galery
         if(requestCode == pickPhotoRequestCode && resultCode == Activity.RESULT_OK && data != null){
             selectedPhoto = data.data
-        }
-        try{
-            context?.let{
-                if(selectedPhoto != null){
-                    //hide the select photo buttons
-                    binding.selectPhotoText.isVisible = false
-                    binding.selectFromGalleryButton.isVisible = false
-                    binding.makePhotoButton.isVisible = false
-
-                    if(Build.VERSION.SDK_INT >= 28){
-                        val source = ImageDecoder.createSource(it.contentResolver, selectedPhoto!!)
-                        selectedBitMap = ImageDecoder.decodeBitmap(source)
-                        binding.selectedPhotoImage.setImageBitmap(selectedBitMap)
-                    }
-                    else{
-                        selectedBitMap = MediaStore.Images.Media.getBitmap(it.contentResolver, selectedPhoto)
-                        binding.selectedPhotoImage.setImageBitmap(selectedBitMap)
-                    }
-                }
+            if(Build.VERSION.SDK_INT >= 28){
+                val source = ImageDecoder.createSource(context?.contentResolver!!, selectedPhoto!!)
+                selectedBitMap = ImageDecoder.decodeBitmap(source)
+                binding.selectedPhotoImage.setImageBitmap(selectedBitMap)
             }
-        }catch (e: Exception){
-            e.printStackTrace()
-            binding.errorMessage.text = e.message
+            else{
+                selectedBitMap = MediaStore.Images.Media.getBitmap(context?.contentResolver!!, selectedPhoto)
+                binding.selectedPhotoImage.setImageBitmap(selectedBitMap)
+            }
+        }
+        //photo from camera
+        else if(requestCode == makePhotoRequestCode && resultCode == Activity.RESULT_OK && data != null){
+            selectedBitMap = data.extras?.get("data") as Bitmap
+            binding.selectedPhotoImage.setImageBitmap(selectedBitMap)
         }
         super.onActivityResult(requestCode, resultCode, data)
     }
