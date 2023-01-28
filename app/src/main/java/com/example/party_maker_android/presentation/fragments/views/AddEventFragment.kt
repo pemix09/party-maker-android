03f06c85ec3763280b721da8f681187c50ce8ee9
pemix09 <pemix09@onet.pl>
2023-01-28 12:43:40 +0100

    Adding events almost works!

--- app/src/main/java/com/example/party_maker_android/presentation/fragments/views/AddEventFragment.kt
+++ app/src/main/java/com/example/party_maker_android/presentation/fragments/views/AddEventFragment.kt
@@ -1,6 +1,5 @@
 package com.example.party_maker_android.presentation.fragments.views
 
-import android.Manifest
 import android.app.Activity
 import android.app.AlertDialog
 import android.content.DialogInterface
@@ -9,24 +8,24 @@ import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.ImageDecoder
 import android.os.Build
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import android.provider.MediaStore
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
-import android.widget.ImageView
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
 import androidx.core.widget.addTextChangedListener
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.ViewModelProvider
 import com.example.party_maker_android.R
 import com.example.party_maker_android.databinding.FragmentAddEventBinding
 import com.example.party_maker_android.presentation.activities.views.AppActivity
 import com.example.party_maker_android.presentation.fragments.viewModels.AddEventViewModel
+
 
 class AddEventFragment : Fragment(), AdapterView.OnItemSelectedListener {
 
